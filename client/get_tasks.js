7905a962c3e13378a267a26f0b02b2ccc51a7956
Diego <ndzerglink@live.jp>
2024-10-24 17:43:28 +0900

    Added function to generate uuid

--- client/get_tasks.js
+++ client/get_tasks.js
@@ -325,6 +325,20 @@ function openNav(){
     // document.querySelector("datalist").style.display="flex";
 }
 
+function gen_uuid() {
+   /* Generate uuid: Since HTTP server doesnt have a security cert*/
+   let uuidValue = "", k, randomValue;
+   for (k = 0; k < 32; k++) {
+       randomValue = (Math.random() * 16) | 0;
+       if (k == 8 || k == 12 || k == 16 || k == 20) {
+           uuidValue += "-";
+       }
+       uuidValue += (
+           k == 12 ? 4 : k == 16 ? (randomValue & 3) | 8 : randomValue
+       ).toString(16);
+   }
+   return uuidValue;
+}
 let formItems=[];
 function validate(e){
     const inps=document.querySelectorAll('input');
